59a505501c1f85b1b3a78fda9959d8357524877b
aladadev <aladadev29@gmail.com>
2026-07-21 22:25:20 +0600

    Link portfolio to GitHub repository

--- portfolio.config.js
+++ portfolio.config.js
@@ -6,7 +6,7 @@ window.portfolioConfig = {
   name: "Al-Amin Sarker",
   email: "aladadev29@gmail.com",
   linkedinUrl: "https://www.linkedin.com/in/md-al-amin-sarker-767b20238/",
-  githubUrl: "https://github.com/your-username",
+  githubUrl: "https://github.com/aladadev/aladadev.github.io",
   featuredProject: {
     demoUrl: "",
     repositoryUrl: "",
